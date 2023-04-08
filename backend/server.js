8f4c05a1e84b66207a4ef612ec0002868c23d96e
wnjenga <wnjenga@safaricom.co.ke>
2023-04-08 23:57:03 +0300

    all files

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,7 @@ const upload = multer();
 const productRoute = require('./routes/api/productRoute');
 
 // Connecting to the Database
-let mongodb_url = 'mongodb://localhost/';
+let mongodb_url = 'mongodb+srv://wnjenga:rlkQEJrPmN7aCPNx@cluster0.exzgxhs.mongodb.net/?retryWrites=true&w=majority/';
 let dbName = 'yolomy';
 
 // define a url to connect to the database
@@ -41,7 +41,7 @@ app.use(cors());
 app.use('/api/products', productRoute)
 
 // Define the PORT
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 3000
 
 app.listen(PORT, ()=>{
     console.log(`Server listening on port ${PORT}`)
